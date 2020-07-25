ace4994aba90af95cea523252f5eaf851427d47f
panlinjun <376151097@qq.com>
2020-07-25 10:36:01 +0800

    A

--- index.js
+++ index.js
@@ -33,48 +33,49 @@ http.createServer(async (req, res) => {
   console.log('receive request')
   console.log(req.url)
   console.log(req.method)
-  // if (req.method === 'POST' && req.url === '/') {
-  //   const data = await resolvePost(req);
-  //   const projectDir = path.resolve(__dirname,`./${data.repository.name}`)
-  //   deleteFolderRecursive(projectDir)
-  //
-  //   // 拉取仓库最新代码
-  //   execSync(`git clone https://github.com/PanLinjun/${data.repository.name}.git ${projectDir}`, {
-  //     stdio: 'inherit',
-  //   })
-  //
-  //   // 复制 Dockerfile 到项目目录
-  //   fs.copyFileSync(path.resolve(__dirname,`./Dockerfile`), path.resolve(projectDir, './Dockerfile'))
-  //
-  //   // 复制 .dockerignore 到项目目录
-  //   fs.copyFileSync(path.resolve(__dirname,`./.dockerignore`), path.resolve(projectDir, './.dockerignore'))
-  //
-  //
-  //   // 创建 docker 镜像
-  //   execSync(`docker build -t ${data.repository.name}-image:latest .`, {
-  //     stdio: 'inherit',
-  //     cwd: projectDir
-  //   })
-  //
-  //   // // 拉取 docker 镜像
-  //   // execSync(`docker pull yeyan1996/docker-test-image:latest`, {
-  //   //     stdio: 'inherit',
-  //   //     cwd: projectDir
-  //   // })
-  //
-  //   // 销毁 docker 容器
-  //   execSync(`docker ps -a -f "name=^${data.repository.name}-container" --format="{{.Names}}" | xargs -r docker stop | xargs -r docker rm`, {
-  //     stdio: 'inherit',
-  //   })
-  //
-  //   // 创建 docker 容器
-  //   execSync(`docker run -d -p 8888:80 --name ${data.repository.name}-container ${data.repository.name}-image:latest`, {
-  //     stdio: 'inherit',
-  //   })
-  //
-  //   console.log('deploy success')
-  // }
-  // res.end('ok')
+  if (req.method === 'POST' && req.url === '/') {
+    const data = await resolvePost(req);
+    const projectDir = path.resolve(__dirname,`./${data.repository.name}`)
+    deleteFolderRecursive(projectDir)
+
+    // 拉取仓库最新代码
+    execSync(`git clone https://github.com/PanLinjun/${data.repository.name}.git ${projectDir}`, {
+      stdio: 'inherit',
+    })
+
+    // 复制 Dockerfile 到项目目录
+    fs.copyFileSync(path.resolve(__dirname,`./Dockerfile`), path.resolve(projectDir, './Dockerfile'))
+    console.log('clone success')
+
+    // 复制 .dockerignore 到项目目录
+    fs.copyFileSync(path.resolve(__dirname,`./.dockerignore`), path.resolve(projectDir, './.dockerignore'))
+
+
+    // 创建 docker 镜像
+    execSync(`docker build -t ${data.repository.name}-image:latest .`, {
+      stdio: 'inherit',
+      cwd: projectDir
+    })
+
+    // // 拉取 docker 镜像
+    // execSync(`docker pull yeyan1996/docker-test-image:latest`, {
+    //     stdio: 'inherit',
+    //     cwd: projectDir
+    // })
+
+    // 销毁 docker 容器
+    execSync(`docker ps -a -f "name=^${data.repository.name}-container" --format="{{.Names}}" | xargs -r docker stop | xargs -r docker rm`, {
+      stdio: 'inherit',
+    })
+
+    // 创建 docker 容器
+    execSync(`docker run -d -p 8888:80 --name ${data.repository.name}-container ${data.repository.name}-image:latest`, {
+      stdio: 'inherit',
+    })
+
+    console.log('deploy success')
+  }
+  res.end('ok')
 }).listen(3000, () => {
   console.log('server is ready')
 })
